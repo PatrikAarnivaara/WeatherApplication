f3de7a6fc8be5af31998aa7485e31bd65798c7d5
Patrik Aarnivaara <patrikaarnivaara@gmail.com>
2021-01-27 13:15:14 +0100

    React native Weather App.

--- App.js
+++ App.js
@@ -1,21 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { LinearGradient } from 'expo-linear-gradient';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<LinearGradient
+				// Background Linear Gradient
+				colors={['#47BFDF', '#4A91FF', 192.05]}
+				style={styles.background}
+			/>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+		backgroundColor: '#fff',
+	},
+	background: {
+		position: 'absolute',
+		left: 0,
+		right: 0,
+		top: 0,
+		height: 700,
+	},
 });

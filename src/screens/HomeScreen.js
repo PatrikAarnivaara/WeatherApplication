71e4f96a649c987ccfde568da15c44ff11a80a8d
Patrik Aarnivaara <patrikaarnivaara@gmail.com>
2021-01-28 10:11:40 +0100

    implementing weather api

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,19 +1,26 @@
 import React from 'react';
-import { LinearGradient } from 'expo-linear-gradient';
 import { Button, StyleSheet, Text, View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+/* import { OPEN_WEATHER_MAP_API_KEY } from '@env' */
 
 const HomeScreen = ({ navigation }) => {
 	return (
 		<View style={styles.container}>
-			<LinearGradient
-				// Background Linear Gradient
-				colors={['#47BFDF', '#4A91FF', 192.05]}
-				style={styles.background}
+			<LinearGradient colors={['#47BFDF', '#4A91FF', 192.05]} style={styles.background} />
+			<Button
+				title={process.env.OPEN_WEATHER_MAP_API_KEY}
+				onPress={() => {
+					/* 1. Navigate to the Details route with params */
+					navigation.navigate('Details', {
+						itemId: 86,
+						otherParam: 'anything you want here',
+					});
+				}}
 			/>
-			<Button title="Go to Details" onPress={() => navigation.navigate('Details')} />
 		</View>
 	);
 };
+
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
